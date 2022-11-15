05d8be685d4cf206a5d59913cd45c179eb79f597
Uhsendin <chilebr99@gmail.com>
2022-11-15 13:47:49 -0500

    removed arrows from input tag

---
+++ app.js
@@ -0,0 +1,10 @@
+const covertBtn = document.getElementById("convert-btn")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+const inputEl = document.getElementById("input-el")
+
+
+ covertBtn.addEventListener("click", function() {
+    lengthEl.textContent = "Yo"
+ })

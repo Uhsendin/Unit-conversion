02d0e5d2ca708be133bbfc8394c3f64c7a4fa07b
Uhsendin <chilebr99@gmail.com>
2022-11-15 14:37:27 -0500

    meters to feet completed

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const covertBtn = document.getElementById("convert-btn")
-const lengthEl = document.getElementById("length-el")
-const volumeEl = document.getElementById("volume-el")
-const massEl = document.getElementById("mass-el")
-const inputEl = document.getElementById("input-el")
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const meterToFeet = 3.281;
+const literToGallon = 0.264;
+const kiloToPound = 2.204;
+const covertBtn = document.getElementById("convert-btn");
+const lengthEl = document.getElementById("length-el");
+const volumeEl = document.getElementById("volume-el");
+const massEl = document.getElementById("mass-el");
+const inputEl = document.getElementById("input-el");
 
 
- covertBtn.addEventListener("click", function() {
-    lengthEl.textContent = "Yo"
- })
+covertBtn.addEventListener("click", function () {
+  let originalValue = inputEl.value;
+  lengthEl.textContent = `${originalValue} meter = ${
+    (originalValue * meterToFeet).toFixed(3)
+  } feet | ${originalValue} feet = ${(originalValue / meterToFeet).toFixed(3)} meters`;
+});
